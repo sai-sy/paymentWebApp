da7d119bb964d5166197acb2cba22302bed2b19a
Sai <saihaansyed2003@gmail.com>
2022-03-27 20:24:52 -0400

    diff

---
+++ wsgi.py
@@ -0,0 +1,17 @@
+from website import create_app, db
+from website.extensions_inits import load_preset_data
+from dataclasses import dataclass
+import imbedded as i
+
+
+def start():
+    app = create_app()
+    app.app_context().push()
+    db.create_all()
+    load_preset_data(app, db)
+
+    return app
+
+if __name__=='__main__':
+    app = start()
+    app.run()

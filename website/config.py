86b465b2ca1df3b8aca0ebab82a4c2862325c700
Sai <saihaansyed2003@gmail.com>
2022-03-28 02:32:41 -0400

    readme

--- website/config.py
+++ website/config.py
@@ -1,13 +1,10 @@
 from os import environ, path
 from dotenv import load_dotenv
 
-
-
 DB_NAME = "main"
 
 class Config:
     """Base config."""
-    SECRET_KEY = environ.get('SECRET_KEY')
     #SESSION_COOKIE_NAME = environ.get('SESSION_COOKIE_NAME')
     MAX_CONTENT_LENGTH = 16*1000*1000
     RECEIPT_FOLDER = '..\\assets\\receipts'
@@ -22,16 +19,17 @@ class ProdConfig(Config):
     FLASK_ENV = 'production'
     DEBUG = False
     TESTING = False
-    DATABASE_URI = env_dict['PROD_DATABASE_URI']
+    SQL_ALCHEMY_DATABASE_URI = env_dict['PROD_DATABASE_URI']
     SECRET_KEY = env_dict['SECRET_KEY']
-
 
 class DevConfig(Config):
     basedir = path.abspath(path.dirname(__file__))
     load_dotenv('C:\saiscripts\intercept_branch\Payment Web App Project\.env')
+    env_dict = dict(environ)
     FLASK_ENV = 'development'
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = environ.get('DEV_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = env_dict['DEV_DATABASE_URI']
+    SECRET_KEY = env_dict['SECRET_KEY']
 
 if __name__ == '__main__':
     print(environ.get('SQLALCHEMY_DATABASE_URI'))

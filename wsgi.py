cb26c5578300fa94641a16e33c179518344a5d66
Sai <saihaansyed2003@gmail.com>
2022-03-28 05:23:20 -0400

    wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,28 @@
-from website import create_app, db
-from website.extensions_inits import load_preset_data
-
-def start():
-    app = create_app()
-    app.app_context().push()
-    db.create_all()
-    load_preset_data(app, db)
-
-    return app
-
 if __name__=='__main__':
+    from website import create_app, db
+    from website.extensions_inits import load_preset_data
+
+    def start():
+        app = create_app()
+        app.app_context().push()
+        db.create_all()
+        load_preset_data(app, db)
+
+        return app
+
     app = start()
-    app.run()
+    app.run(host='0.0.0.0')
+else:
+    from .website import create_app, db
+    from .website.extensions_inits import load_preset_data
+
+    def start():
+        app = create_app()
+        app.app_context().push()
+        db.create_all()
+        load_preset_data(app, db)
+
+        return app
+
+    app = start()
+    app.run(host='0.0.0.0')

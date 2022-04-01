1c0d09e03d0b27a6d7779943ea2e7bd75dcb50cd
Sai <saihaansyed2003@gmail.com>
2022-04-01 05:09:40 -0400

    next

--- website/auth.py
+++ website/auth.py
@@ -35,7 +35,7 @@ def login():
                 next = request.args.get('next')
                 # is_safe_url should check if the url is safe for redirects.
                 # See http://flask.pocoo.org/snippets/62/ for an example.
-                return redirect(url_for('views.' + next[2:] or 'views.home'))
+                return redirect(url_for('views.home' or next))
             else:
                 form.email.data = ''
                 form.password.data = ''

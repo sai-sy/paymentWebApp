e2300f054b556829c823471f2a508ca677e7fbc3
Sai <saihaansyed2003@gmail.com>
2022-04-01 21:05:59 -0400

    next test

--- website/auth.py
+++ website/auth.py
@@ -43,7 +43,7 @@ def login():
                 form.password.data = ''
                 
                 #Default
-                #return redirect(url_for('views.home'))
+                return redirect(url_for('views.home'))
 
                 # Method Two
                 next = request.args.get('next')

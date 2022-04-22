1cc6661002edeeeff74f1e94b77fc2d4126fd8be
Sai <saihaansyed2003@gmail.com>
2022-04-22 13:06:32 -0400

    campaigns

--- website/models/campaigns.py
+++ website/models/campaigns.py
@@ -215,7 +215,13 @@ class Campaigns(db.Model):
     def process_new_payment(self, paystamp: PayStamps):
         user_contract: Campaign_Contracts = Campaign_Contracts.query.filter_by(user_id=paystamp.user_id, campaign_id=self.id).first()
         user_contract.pay_out['paid']['paystamps']['total'] += paystamp.amount
-        user_contract.pay_out['paid']['paystamps'][paystamp.activity_id] += paystamp.amount
+        try:
+            user_contract.pay_out['paid']['paystamps'][paystamp.activity_id] += paystamp.amount
+        except KeyError:
+            user_contract.pay_out['paid']['paystamps'][paystamp.activity_id] = 0
+        
+        user_contract.pay_out['owed']['untrunced_sum'] = user_contract.pay_out['earnings'] - user_contract.pay_out['paid']['paystamps']['total']
+
         db.session.commit()
 
     def process_pay(self):
